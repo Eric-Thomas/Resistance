f2ef67a7943351207c9a132cb06981e81372935b
James  Wallace <jameswallace@Jamess-MBP.lan1>
2020-08-15 16:04:29 -0400

    add register crispy forms

--- resistance/users/views.py
+++ resistance/users/views.py
@@ -1,8 +1,12 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
-
-# Create your views here.
 
 
 def register(request):
+    form = UserCreationForm()
 
-    return render(request, 'users/register.html')
+    context = {
+        'form': form
+    }
+
+    return render(request, 'users/register.html', context)
