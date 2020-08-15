f38dcd73f068fbf37e79c894edd8a71e1289f97a
James  Wallace <jameswallace@Jamess-MBP.lan1>
2020-08-15 16:30:06 -0400

    posted to database

--- resistance/users/views.py
+++ resistance/users/views.py
@@ -9,6 +9,9 @@ def register(request):
         form = UserCreationForm(request.POST)
 
         if form.is_valid():
+
+            form.save()
+
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
 
